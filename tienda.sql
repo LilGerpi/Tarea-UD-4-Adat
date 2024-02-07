4132a8a089e0dd341288c78460848fb48fd5b009
Daniel Souto Gerpe <116888528+LilGerpi@users.noreply.github.com>
2024-02-07 09:31:04 +0100

    Actualizacion SQL

--- tienda.sql
+++ tienda.sql
@@ -3,9 +3,11 @@ CREATE DATABASE tienda;
 USE tienda;
 
 CREATE TABLE productos (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   precio DECIMAL(10, 2) NOT NULL,
   cantidad INT NOT NULL,
   categoria VARCHAR(255)
 );
+
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `cantidad`, `categoria`) VALUES (NULL, 'Patatas', '2.00', '1', 'Verduras');
